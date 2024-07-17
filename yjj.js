b2b5f33fb488d6ba3d4796d372b1e1e93abba8fa
kuangshouxia <aafa8146@gmail.com>
2024-07-17 20:44:36 +0800

    yjj.js

---
+++ yjj.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6122e24e4945f87aef1fda2881ef70c3";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
